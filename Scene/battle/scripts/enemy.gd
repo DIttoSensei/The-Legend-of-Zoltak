c3208a886a043001a4fde00d1eb51588aa2348cd
DIttoSensei <richardtommandrew@gmail.com>
2025-10-08 12:54:23 +0100

    Battle scene update

--- Scene/battle/scripts/enemy.gd
+++ Scene/battle/scripts/enemy.gd
@@ -3,15 +3,20 @@ class_name Enemy extends Sprite2D
 var atk : int
 var def : int
 var dex : int
+var enemy_damage
+var current_hp
 
 @export var enemy_data : Enemies_res
 
 @onready var enemy_hp: TextureProgressBar = $"../enemy_hp"
+@onready var player: Player = $"../player"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	set_enemy_data()
+	
+	SignalManager.enemy_damaged.connect(take_damage)
 	pass # Replace with function body.
 
 
@@ -19,6 +24,7 @@ func set_enemy_data () -> void:
 	# set the HP
 	enemy_hp.max_value = enemy_data.hp
 	enemy_hp.value = enemy_hp.max_value
+	current_hp = enemy_hp.value
 	
 	# set the stat
 	atk = enemy_data.atk
@@ -67,3 +73,24 @@ func attack_player () -> void:
 			var enemy_name = enemy_data.name
 			SignalManager.enemy_attack_data.emit(enemy_name, move_name, damage, anim_name)
 			break
+
+func take_damage (damage : int) -> void:
+	enemy_damage = damage
+	pass
+
+func _on_hitbox_area_entered(_area: Area2D) -> void:
+	$AnimationPlayer.play("hit")
+	$"../enemy_dmg hit".text = str (enemy_damage)
+	$emeny_dmg.play("dmg")
+	current_hp -= enemy_damage
+	enemy_hp.value = current_hp
+	
+	await get_tree().create_timer(0.5).timeout
+	player.set_battle_stat()
+	pass # Replace with function body.
+
+
+func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
+	$AnimationPlayer.play("idle")
+	$AnimationPlayer.seek(0.0, true)
+	pass # Replace with function body.
