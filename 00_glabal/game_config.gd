33acb66920ef26d36a5ee20037b6e70dde929601
DIttoSensei <104984682+DIttoSensei@users.noreply.github.com>
2026-01-01 17:25:17 +0100

    atk and def item buff added

---
+++ 00_glabal/game_config.gd
@@ -0,0 +1,19 @@
+extends Node
+
+## GAME CONFIG FOR ITEM BUFFS
+var atk_buff_value : int = 0
+var def_buff_value : int = 0
+var crit_buff_value : int = 0
+var fire_buff_value : int = 0
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
